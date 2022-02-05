57b8d1b0851573da8a8c9ae6631c154078896edd
Ode42 <ode.progr@outlook.com>
2022-02-05 21:31:58 +0200

    Tracking sijoitussumma changes

--- src/components/InputPanel.jsx
+++ src/components/InputPanel.jsx
@@ -1,11 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export default function InputPanel(props) {
   const [sijoitussumma, setSijoitussumma] = useState(0);
 
   const sijoitussummaUpdate = () => {
-    props.addSijoitussumma(sijoitussumma);
+    props.callback(sijoitussumma);
   };
+
+  useEffect(() => {
+    sijoitussummaUpdate();
+  }, [sijoitussumma]);
+
   return (
     <div className="input-panel">
       <h2>Input panel</h2>
@@ -14,7 +19,9 @@ export default function InputPanel(props) {
         <input
           type="number"
           value={sijoitussumma}
-          onChange={(e) => setSijoitussumma(e.target.value)}
+          onChange={(e) => {
+            setSijoitussumma(e.target.value);
+          }}
         />
         <button type="button" onClick={sijoitussummaUpdate}>
           Lähetä
